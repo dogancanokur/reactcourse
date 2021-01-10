5f3be5d6e828830eb55a5b4e11bb13ec2c9355d5
dogan.okur <dogan.okur@hititcs.com>
2021-01-11 00:30:39 +0300

    intro componentler

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h2>App</h2>
+
+            <Navi/>
+            <CategoryList/>
+            <ProductList/>
+        </div>
+    );
 }
 
 export default App;
