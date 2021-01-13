13b797c87ffb64e4741d31c91bc88e10b0291a2c
dogan.okur <dogan.okur@hititcs.com>
2021-01-14 02:02:17 +0300

    form submit

--- intro/src/FormDemo1.js
+++ intro/src/FormDemo1.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Form} from "reactstrap";
 
 export default class FormDemo1 extends Component {
     state = {
@@ -10,14 +9,20 @@ export default class FormDemo1 extends Component {
         this.setState({username: event.target.value});
     }
 
+    onSubmitHandler = (event) => {
+        event.preventDefault(); // default davranisi uygulama
+        alert(this.state.username);
+    }
+
     render() {
         return (
             <div>
-                <Form>
+                <form onSubmit={this.onSubmitHandler}>
                     <h3>Username</h3>
                     <input type="text" onChange={this.onChangeHandler}/>
                     <h3>Username is {this.state.username}</h3>
-                </Form>
+                    <input type="submit" value="Save"/>
+                </form>
             </div>
         );
     }
