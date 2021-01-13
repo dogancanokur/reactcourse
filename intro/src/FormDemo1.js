45c1493a76d513ebd8e412fb829d359feb92251c
dogan.okur <dogan.okur@hititcs.com>
2021-01-14 02:08:10 +0300

    coklu input submit

--- intro/src/FormDemo1.js
+++ intro/src/FormDemo1.js
@@ -2,26 +2,37 @@ import React, {Component} from 'react';
 
 export default class FormDemo1 extends Component {
     state = {
-        username: ''
+        username: '',
+        city: ''
     }
 
     onChangeHandler = (event) => {
-        this.setState({username: event.target.value});
+        // this.setState({username: event.target.value});
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]: value});
     }
 
     onSubmitHandler = (event) => {
         event.preventDefault(); // default davranisi uygulama
-        alert(this.state.username);
+        alert(this.state.username, ' ', this.state.city);
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.onSubmitHandler}>
+
                     <h3>Username</h3>
-                    <input type="text" onChange={this.onChangeHandler}/>
+                    <input type="text" onChange={this.onChangeHandler} name="username"/>
                     <h3>Username is {this.state.username}</h3>
+
+                    <h3>City</h3>
+                    <input type="text" onChange={this.onChangeHandler} name="city"/>
+                    <h3>City is {this.state.city}</h3>
+
                     <input type="submit" value="Save"/>
+
                 </form>
             </div>
         );
