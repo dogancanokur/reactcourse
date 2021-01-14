fe5a40b5544c200513b993c50e1ff67b6a8e5506
dogancanokur <ahmet.okur.94@gmail.com>
2021-01-15 02:11:43 +0300

    react redux - component store bağlantısı

--- redux-intro/src/App.js
+++ redux-intro/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import IncreaseCounter from "./components/IncreaseCounter";
+import Counter from "./components/Counter";
+import DecreaseCounter from "./components/DecreaseCounter";
+import IncreaseByTwoCounter from "./components/IncreaseByTwoCounter";
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <div>
+            <Counter/>
+            <IncreaseCounter/>
+            <DecreaseCounter/>
+            <IncreaseByTwoCounter/>
+        </div>
+    );
 }
 
 export default App;
