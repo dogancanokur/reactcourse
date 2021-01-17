193ab4c2578aba94ab63d596d0958607ed79f3da
dogancanokur <ahmet.okur.94@gmail.com>
2021-01-17 06:14:08 +0300

    redux store config

--- northwind-redux/src/components/categories/CategoryList.js
+++ northwind-redux/src/components/categories/CategoryList.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 
 class CategoryList extends Component {
     render() {
         return (
             <div>
                 <h3>Category List</h3>
+                <h5>Seçili Kategori : {this.props.currentCategory.categoryName}</h5>
             </div>
         );
     }
 }
 
-export default CategoryList;
+function mapStateToProps(state) {
+    return {
+        currentCategory: state.changeCategoryReducer
+    }
+}
+
+export default connect(mapStateToProps)(CategoryList);
